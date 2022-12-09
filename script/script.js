733314f07ae479234641ed42d740dcb494416dfb
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2022-12-09 21:06:29 +0000

    Timeblock code css mostly sorted. File structure amendeded

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const currentDayJumbo = document.querySelector("#currentDay");
+
+//Jumbotron:
+//date element added to it. Dependant on moment.js
+
+//Timeblocks section:
+//addeventlistener to container, if target = one of the textblocks update storage for relevant.
