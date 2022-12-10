cda7b00cbd402e4550516df1e9c6616d3e2f4970
kieran_parkinson@hotmail.com <kieran_parkinson@hotmail.com>
2022-12-10 01:59:44 +0000

    added time display in jumbotron

--- script/script.js
+++ script/script.js
@@ -1,7 +1,22 @@
+const jumboTime = document.querySelector("#jumboTime");
 const currentDayJumbo = document.querySelector("#currentDay");
+const container = document.querySelector(".container");
+let time = 0;
 
 //Jumbotron:
 //date element added to it. Dependant on moment.js
 
 //Timeblocks section:
 //addeventlistener to container, if target = one of the textblocks update storage for relevant.
+
+console.log(time);
+container.addEventListener("click", function (e) {
+  console.log(e);
+  console.log(e.target);
+});
+
+setInterval(function () {
+  time = moment().format("hh.mm a");
+  jumboTime.textContent = time;
+}),
+  1000;
